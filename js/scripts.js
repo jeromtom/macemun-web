503a1aa93a9177c163b8467565807e8df3fe2435
futuretechie4u-collab <futuretechie4u@gmail.com>
2025-12-27 06:11:51 +0530

    changes in the font navbar in pages added image for carousel

--- js/scripts.js
+++ js/scripts.js
@@ -102,3 +102,16 @@ document.addEventListener('DOMContentLoaded', function() {
     startCountdown(eventDate);
 });
 
+// document.addEventListener("keydown", function (e) {
+//     if (
+//       e.ctrlKey && (e.key === 's' || e.key === 'u' || e.key === 'c') ||
+//       e.key === 'F12'
+//     ) {
+//       e.preventDefault();
+//       return false;
+//     }
+//   });
+//   document.addEventListener("contextmenu", function (e) {
+//     e.preventDefault();
+//   });
+
